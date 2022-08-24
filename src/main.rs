6dfea37472409194309bd00c8c806542f19cf386
Peter Meissner <retep.meissner@gmail.com>
2022-08-24 23:01:39 +0200

    tutorial: plugins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use bevy::prelude::*;
+mod h;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+  commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+  commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+  commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+
+fn hello_world() {
+  println!("hello world!");
+}
+
+pub struct HelloPlugin;
+
+
+impl Plugin for HelloPlugin {
+  fn build(&self, app: &mut App) {
+      app.add_startup_system(add_people)
+          .add_system(hello_world)
+          .add_system(greet_people);
+  }
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+  for name in query.iter() {
+      println!("hello {}!", name.0);
+  }
+}
+
 fn main() {
-    println!("Hello, world, thing!");
+  App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugin(HelloPlugin)
+    .run();
 }
